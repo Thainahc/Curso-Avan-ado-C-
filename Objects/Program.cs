0d92c8bbc46779f0a2952e5b639ef28f9d392051
Thainá <Thain@DESKTOP-40P3NL4>
2016-11-23 23:05:11 -0200

    Refereciando objetos

---
+++ Objects/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class Program
+    {
+        public class Cliente : object // Reference Type
+        {
+            public int Codigo;
+            public string Nome;
+            public string Telefone;
+            public override string ToString()
+            {
+                StringBuilder string1 = new StringBuilder();
+                string1.AppendLine("Codigo: " + Codigo);
+                string1.AppendLine("Nome: " + Nome);
+                string1.AppendLine("Telefone: " + Telefone);
+
+                return string1.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente();
+            Cliente cliente2 = cliente1;
+            //Cliente cliente2 = new Cliente();
+
+            cliente1.Codigo = 1;
+            cliente1.Nome = "Pudim";
+            cliente1.Telefone = "9898998";
+
+            cliente2.Nome = "Café";
+
+            Console.WriteLine(cliente1);
+            Console.WriteLine(cliente2);
+
+            if (cliente1.Equals(cliente2))
+            {
+                Console.WriteLine("cliente1 e cliente2 são os mesmos objetos");
+            }
+            else
+            {
+                Console.WriteLine("cliente1 e cliente2 são objetos diferentes");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
